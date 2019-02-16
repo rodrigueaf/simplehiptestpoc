cfb79a12ec57c88e1780f535df5474f830c89a07
rodrigue <affodogandji.rodrigue@gmail.com>
2019-02-16 23:45:40 +0100

    Ajout du home link

--- src/main/java/com/rodrigue/simplehiptestpoc/ServiceApplication.java
+++ src/main/java/com/rodrigue/simplehiptestpoc/ServiceApplication.java
@@ -1,11 +1,20 @@
 package com.rodrigue.simplehiptestpoc;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ServiceApplication {
 
     public static void main(String[] args){
+        SpringApplication.run(ServiceApplication.class, args);
+    }
 
+    @GetMapping("/")
+    public String home(){
+        return "Welcome";
     }
 }
